48b27d61cf7d8bfcb5f9fc5144ff8304c3115aae
kpjha786 <kpjha786@gmail.com>
2022-01-02 18:14:18 +0530

    Todo App

---
+++ script.js
@@ -0,0 +1,41 @@
+// Naming the ids as variables
+let ulTasks=$('#ulTasks');
+let btnAdd=$('#btnAdd');
+let btnReset=$('#btnReset');
+let btnSort=$('#btnSort');
+let btnCleanup=$('#btnCleanup');
+let inpNewTask=$('#inpNewTask');
+
+function addItem(){
+    let todo=inpNewTask.val();
+    console.log("clicked");
+    ulTasks.append('<li>'+todo+'</li>');
+    inpNewTask.val("");
+}
+
+inpNewTask.keypress(function(e){
+    // console.log("clicked");
+    if(e.which==13)
+        addItem();
+});
+
+// selects individual from the lists
+ulTasks.on("click","li",function(){
+    $(this).toggleClass('done');
+});
+
+btnAdd.click(addItem);
+
+btnReset.click(function(){
+    inpNewTask.val("");
+});
+btnCleanup.click(function(){
+    ulTasks.empty();
+    inpNewTask.val("");
+});
+
+btnSort.click(sortTasks);
+function sortTasks() {
+    $('.done').appendTo(ulTasks);  //appenTo after ->append does not work
+  }
+  
