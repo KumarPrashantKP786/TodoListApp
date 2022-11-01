4201a2b986f547378e7891829a86314d4701b11e
kpjha786 <kpjha786@gmail.com>
2022-11-02 01:52:27 +0530

    psuh

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let ulTasks=$('#ulTasks');
 let btnAdd=$('#btnAdd');
 let btnReset=$('#btnReset');
-let btnSort=$('#btnSorting');
+let btnSort=$('#btnSortingabc');
 let btnCleanup=$('#btnCleanup');
 let inpNewTask=$('#inpNewTask');
 let abcdef=123;
